41e280571367e296dea447e582858cc3b047ee91
InvalidArgument3 <InvalidArgument3@gmail.com>
2025-07-21 07:55:13 -0500

    waystone 10lvl globally

--- kubejs/server_scripts/_cabin_motd.js
+++
@@ -1,4 +0,0 @@
-PlayerEvents.loggedIn(event => {
-    event.player.tell(["Welcome to ", Text.gold("CABIN"), " on 1.20"]);
-    event.player.tell(["Report pack issues to ", Text.gold("the Github").underlined().clickOpenUrl("https://github.com/ThePansmith/CABIN").hover("Click to open"), "."]);
-})
